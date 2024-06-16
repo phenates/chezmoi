e92d76c145b0aba1f97defc3c99b671ce816730c
phenates <phen4tes@gmail.com>
2024-06-16 07:47:26 +0200

    wip install

--- install.sh
+++ install.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
+# Install chezmoi, init and apply
 # Run: sh -c "$(wget -qO- https://raw.githubusercontent.com/phenates/dotfiles/master/install.sh)"
 
-echo "Hello world"
+set -e
+echo ""
+echo "------ dotfiles management ------"
+
+if [ ! "$(chezmoi -v)" ]; then
+  echo "chezmoi installation"
+  if [ "$(wget --version)" ]; then
+    sh -c "$(wget -qO- get.chezmoi.io/lb)"
+  elif [ "$(curl -v)" ]; then
+    sh -c "$(curl -fsLS get.chezmoi.io/lb)"
+  else
+    echo "To install chezmoi, you must have curl or wget installed." >&2
+    exit 1
+  fi
+else
+  echo "chezmoi seems to be already installed"
+fi
