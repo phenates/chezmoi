ccbb8d80df6bfc5a8fbc39d19d3e7f9a3d723e0b
phenates <phen4tes@gmail.com>
2024-06-17 05:27:40 +0200

    change test condition

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# Install chezmoi, init and apply
+# Install chezmoi and/or init and apply
 # Run: sh -c "$(wget -qO- https://raw.githubusercontent.com/phenates/dotfiles/master/install.sh)"
 
-set -e
+set -e # -e: exit on error
 
 NOCOLOR=$(tput sgr0)
 CYAN=$(tput setaf 6)
@@ -11,16 +11,33 @@ CYAN=$(tput setaf 6)
 echo ""
 echo $CYAN">>>>> dotfiles management <<<<<"$NOCOLOR
 
-if [ ! "$(chezmoi --version)" ]; then
-  echo $CYAN">>> chezmoi installation"$NOCOLOR
-  if [ "$(wget --version)" ]; then
-    sh -c "$(wget -qO- get.chezmoi.io/)"
-  elif [ "$(curl -v)" ]; then
-    sh -c "$(curl -fsLS get.chezmoi.io/)"
+if [ ! "$(command -v chezmoi)" ]; then
+  echo $CYAN"--> chezmoi installation"$NOCOLOR
+  bin_dir="$HOME/.local/bin"
+
+  echo $GREEN"install option: [1]: install (default); [2]: install and init; [3]: install, init and apply"
+  case "$REPLY" in
+  2)
+    OPTION="-- init phenates"
+    ;;
+  3)
+    OPTION="-- init --apply phenates"
+    ;;
+  *)
+    OPTION=""
+    ;;
+  esac
+
+  if [ "$(command -v wget)" ]; then
+    sh -c "$(wget -qO- get.chezmoi.io/)" $OPTION
+  elif [ "$(command -v curl)" ]; then
+    sh -c "$(curl -fsLS get.chezmoi.io/)" $OPTION
   else
-    echo $CYAN">>> To install chezmoi, you must have curl or wget installed."$NOCOLOR
+    echo $CYAN"--> To install chezmoi, you must have curl or wget installed."$NOCOLOR
     exit 1
   fi
 else
-  echo $CYAN">>> chezmoi seems to be already installed"$NOCOLOR
+  echo $CYAN"--> chezmoi seems to be already installed"$NOCOLOR
 fi
+
+echo $GREEN""
